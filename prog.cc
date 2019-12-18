b3dcfac02b172d89c2b327160b38efe0db2a3cb7
JUAN CARLOS MAESTRE MARTINEZ <jcmm17@clL23-4>
2019-12-18 15:57:02 +0100

    Añadido codigo calcula numeros perfectos

---
+++ prog.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=1,x=1,s=0;
+    while(n<=10000)
+    {
+        while(x<=(n/2))
+        {
+            if(n%x==0)
+                s=s+x;
+                x=x+1;
+        }
+        if(n==s)
+            cout<<n<<", Es perfecto"<<endl;
+        n=n+1; x=1;s=0;
+    }
+    cin.ignore(); return 0;
+}
+//UN NUMERO PERFECTO ES CUANDO TODOS SUS DIVISORES MENORES SUMADOS DAN EL MISMO NUMERO.
+
